409d95b49b27e94c5e49f778140f78fdf5624c6a
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-03-03 15:07:34 -0800

    Some modularization and setup increment route

--- src/api.js
+++ src/api.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var CountModel = require('./database');
 
-//setup db connection
-mongoose.connect('mongodb://localhost:27017/GIFSearch', {useNewUrlParser: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected');
-});
-
-//create schema and model
-var countSchema = new mongoose.Schema({
-  count: Number
-});
-var CountModel = mongoose.model('Count', countSchema, 'Count');
-
+//redirect to count
 router.get('/', function(req, res, next) {
+  res.redirect('/api/count');
+});
+
+//show count
+router.get('/count', function(req, res, next) {
   CountModel.findOne({}).exec(function(err, count) {
+    if (err) console.log('failure');
+    res.json({count});
+  });
+});
+
+//increment count
+router.get('/increment', function(req, res, next) {
+  var query = { _id: '5e5ed0bed3fafa07980ff4ab' };
+  CountModel.findOneAndUpdate(query, { $inc: {count: 1} }).exec(function(err, count) {
     if (err) console.log('failure');
     res.json({count});
   });
---
+++ src/database.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+//setup db connection
+mongoose.connect('mongodb://localhost:27017/GIFSearch', {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected');
+});
+
+//create schema and model
+var countSchema = new mongoose.Schema({
+    count: Number
+});
+var CountModel = mongoose.model('Count', countSchema, 'Count');
+
+module.exports = CountModel;
