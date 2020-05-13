68e249f8d4f56de00e0b3fac3382b1fabac1b9d6
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-05-13 12:02:50 -0700

    Add db connection

--- src/database.js
+++ src/database.js
@@ -1,1 +1,22 @@
-//TODO connect to azure sql db
+var sql = require('mssql');
+
+const db = new sql.ConnectionPool({
+    user: 'carmendb',
+    password: 'twoShoe22!',
+    server: 'goalmine20200220070938dbserver.database.windows.net',
+    database: 'GoalMine20200220070938_db',
+    options: {
+        enableArithAbort: true //abort fatal errors
+    }
+});
+
+db.connect(err => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('connected')
+    }
+});
+
+module.exports = db;
