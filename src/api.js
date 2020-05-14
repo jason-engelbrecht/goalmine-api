62894e0a7697e49ecaad554ca8a13dbdeb8fe721
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-05-14 00:27:55 -0700

    Create database class and try out mssql

--- src/api.js
+++ src/api.js
@@ -1,10 +1,23 @@
 import express from 'express';
-import db from './database';
+import Database from './database';
 
 const router = express.Router();
+const db = new Database();
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello world');
+});
+
+router.get('/users', (req, res) => {
+    db.getAllUsers((users) => {
+        res.send(users);
+    });
+});
+
+router.get('/parents', (req, res) => {
+    db.getAllParents((parents) => {
+        res.send(parents);
+    });
 });
 
 export default router;
--- src/database.js
+++ src/database.js
@@ -1,22 +1,53 @@
 import sql from 'mssql';
 
-const db = new sql.ConnectionPool({
-    user: 'carmendb',
-    password: 'twoShoe22!',
-    server: 'goalmine20200220070938dbserver.database.windows.net',
-    database: 'GoalMine20200220070938_db',
-    options: {
-        enableArithAbort: true //abort fatal errors
-    }
-});
+class Database {
+    pool = new sql.ConnectionPool({
+        user: 'carmendb',
+        password: 'twoShoe22!',
+        server: 'goalmine20200220070938dbserver.database.windows.net',
+        database: 'GoalMine20200220070938_db',
+        options: {
+            enableArithAbort: true //abort fatal errors
+        }
+    });
 
-db.connect(err => {
-    if(err) {
-        console.log(err);
+    constructor() {
+        try {
+            this.connect();
+        }
+        catch(err) {
+            console.error(err);
+        }
     }
-    else {
-        console.log('connected')
-    }
-});
 
-export default db;
+    connect() {
+        this.pool.connect(err => {
+            if(err) throw err;
+            console.log('connected');
+        });
+    }
+
+    //basic request
+    getAllUsers(callback) {
+        const request = this.pool.request();
+
+        request.query('SELECT * from UserAccount', (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        });
+    }
+
+    //request w input parameter
+    getAllParents(callback) {
+        const request = this.pool.request();
+        request.input('parent', sql.Bit, 1);
+        const query = 'SELECT * FROM UserAccount WHERE Parent = @parent';
+
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        });
+    }
+}
+
+export default Database;
