c1ea07ad04f0818a283b0f2434eeaffa80c43be7
Vera Mankongvanichkul <verawun@gmail.com>
2020-05-16 21:00:24 -0700

    add api routes for all tables and objective details

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 import sql from 'mssql';
 import bcrypt from 'bcrypt';
+import router from "./api";
 
 class Database {
     pool = new sql.ConnectionPool({
@@ -59,11 +60,137 @@ class Database {
         })
     }
 
+    getAllGoals(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Goal';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    getAllNotes(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Note';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    getAllObjectives(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Objective';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    getAllScores(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Scores';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    getAllTrials(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Trial';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    getAllEvidence(callback) {
+        const request = this.pool.request();
+        const query = 'SELECT * FROM Evidence';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
     getParentChildren(parentId, callback) {
         console.log("parentId: " + parentId);
         const request = this.pool.request();
         request.input('parent', sql.Int, parentId);
         const query = 'SELECT * FROM Student WHERE ParentId = @parent';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets students goals
+    getStudentGoals(studentId, callback) {
+        console.log("studentId: " + studentId);
+        const request = this.pool.request();
+        request.input('student', sql.Int, studentId);
+        const query = 'SELECT * FROM Goal WHERE StudentId = @student';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets goal objectives
+    getGoalObjectives(goalId, callback) {
+        console.log("goalId: " + goalId);
+        const request = this.pool.request();
+        request.input('goal', sql.Int, goalId);
+        const query = 'SELECT * FROM Objective WHERE goalId = @goal';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets objective notes
+    getObjectiveNotes(objectiveId, callback) {
+        console.log("objectiveId: " + objectiveId);
+        const request = this.pool.request();
+        request.input('objective', sql.Int, objectiveId);
+        const query = 'SELECT * FROM Note WHERE objectiveId = @objective';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets objective scores
+    getObjectiveScores(objectiveId, callback) {
+        console.log("objectiveId: " + objectiveId);
+        const request = this.pool.request();
+        request.input('objective', sql.Int, objectiveId);
+        const query = 'SELECT * FROM Scores WHERE objectiveId = @objective';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets objective trials
+    getObjectiveTrials(objectiveId, callback) {
+        console.log("objectiveId: " + objectiveId);
+        const request = this.pool.request();
+        request.input('objective', sql.Int, objectiveId);
+        const query = 'SELECT * FROM Trial WHERE objectiveId = @objective';
+        request.query(query, (err, result) => {
+            if(err) throw err;
+            callback(result.recordset);
+        })
+    }
+
+    //gets objective evidence
+    getObjectiveEvidence(objectiveId, callback) {
+        console.log("objectiveId: " + objectiveId);
+        const request = this.pool.request();
+        request.input('objective', sql.Int, objectiveId);
+        const query = 'SELECT * FROM Evidence WHERE objectiveId = @objective';
         request.query(query, (err, result) => {
             if(err) throw err;
             callback(result.recordset);
@@ -87,7 +214,7 @@ class Database {
         });
     }
 
-    //TODO get students & there goals (INNER JOIN), based on parents ID from login
+    //TODO get students & their goals (INNER JOIN), based on parents ID from login
 
     //TODO get all objectives based on goal ids from ^
 }
